a02d38e0ac966f14e714a3f5287b722e88f2e15b
Greg Keele <gkeele@gmail.com>
2017-05-19 15:40:00 -0400

    Adding documentation to threshold functions

--- R/haplotype_thresholds.R
+++ R/haplotype_thresholds.R
@@ -7,7 +7,7 @@
 #' @param use.REML DEFAULT: TRUE. Determines whether the variance components for the parametric sampling are 
 #' based on maximizing the likelihood (ML) or the residual likelihood (REML).
 #' @param num.samples The number of parametric bootstrap samples to return.
-#' @param seed DEFAULT: 1. The sampling process is random, thus a seed must be set for samples to be the same
+#' @param seed DEFAULT: 1. The sampling process is random, thus a seed must be set for samples to be consistent
 #' across machines.
 #' @export
 #' @examples generate.null.bootstrap.matrix()
@@ -62,7 +62,20 @@ generate.null.bootstrap.matrix <- function(scan.object, use.REML=TRUE, num.sampl
   return(sim.threshold.object)
 }
 
+#' Returns a matrix of (parametric) permutaions of the outcome
+#'
+#' This function takes an scan.h2lmm() object, and returns a specified number of permutation samples. When the
+#' the null model contains covariates or there is a genetic relationship matrix, permutations are returned based
+#' the maintains the structure of the data according to the null model.
+#'
+#' @param scan.object A scan.h2lmm() object.
+#' @param use.REML DEFAULT: TRUE. Determines whether the variance components for the parametric sampling are 
+#' based on maximizing the likelihood (ML) or the residual likelihood (REML).
+#' @param num.samples The number of permutation samples to return.
+#' @param seed DEFAULT: 1. The sampling process is random, thus a seed must be set for samples to be consistent
+#' across machines.
 #' @export
+#' @examples generate.perm.matrix()
 generate.perm.matrix <- function(scan.object, use.REML=TRUE, num.samples, seed=1){
   if(class(scan.object$fit0) != "lmerMod"){
     Xb <- scan.object$fit0$x %*% scan.object$fit0$coefficients
@@ -115,13 +128,34 @@ generate.perm.matrix <- function(scan.object, use.REML=TRUE, num.samples, seed=1
   return(sim.threshold.object)
 }
 
+#' Runs threshold scans from a matrix of outcomes, either parametric bootstraps from the null model or permutations
+#'
+#' This function takes an object produced from either generate.null.bootstrap.matrix() or generate.perm.matrix(), and 
+#' runs genome scans on the outcomes contained in them.
+#'
+#' @param sim.threshold.object An object created by either generate.null.bootstrap.matrix() or generate.perm.matrix().
+#' @param keep.full.scans DEFAULT: TRUE. Returns full genome scans for every outcome sample in the sim.threshold.object. Can be used
+#' for visualization of the procedure, but greatly increases the size of the output object.
+#' @param genomecache The path to the genome cache directory. The genome cache is a particularly structured
+#' directory that stores the haplotype probabilities/dosages at each locus. It has an additive model
+#' subdirectory and a full model subdirectory. Each contains subdirectories for each chromosome, which then
+#' store .RData files for the probabilities/dosages of each locus.
+#' @param data A data frame with outcome and potential covariates. Should also have IDs
+#' that link to IDs in the genome cache, often the individual-level ID named "SUBJECT.NAME".
+#' @param model DEFAULT: additive. Specifies how to model the founder haplotype probabilities. The additive options specifies
+#' use of haplotype dosages, and is most commonly used. The full option regresses the phenotype on the actual
+#' diplotype probabilities. The diplolasso option specifies the DiploLASSO model.
+#' @param use.multi.impute DEFAULT: TRUE. This option specifies whether to use ROP or multiple imputations.
+#' @param num.imp DEFAULT: 11. IF multiple imputations are used, this specifies the number of imputations to perform.
+#' @param scan.seed DEFAULT: 1. The sampling process is random, thus a seed must be set for samples to be consistent
+#' across machines.
 #' @export
+#' @examples run.threshold.scans()
 run.threshold.scans <- function(sim.threshold.object, keep.full.scans=TRUE,
                                 genomecache, data,
                                 model=c("additive", "full", "diplolasso"),
-                                use.par="h2", use.multi.impute=TRUE, num.imp=10, brute=TRUE, use.fix.par=FALSE, 
-                                scan.seed=1, do.augment=FALSE, chr="all",
-                                use.augment.weights=FALSE, use.full.null=FALSE, added.data.points=1, ...){
+                                use.multi.impute=TRUE, num.imp=11, 
+                                scan.seed=1, ...){
   y.matrix <- sim.threshold.object$y.matrix
   formula <- sim.threshold.object$formula
   weights <- sim.threshold.object$weights
@@ -153,8 +187,8 @@ run.threshold.scans <- function(sim.threshold.object, keep.full.scans=TRUE,
     this.data <- merge(x=new.y, y=data, by="SUBJECT.NAME", all.x=TRUE)
 
     this.scan <- scan.h2lmm(genomecache=genomecache, data=this.data, formula=iteration.formula, K=K, model=model,
-                            use.par=use.par, use.multi.impute=use.multi.impute, num.imp=num.imp, chr=chr, brute=brute, use.fix.par=use.fix.par, seed=scan.seed, do.augment=do.augment, 
-                            weights=weights, use.augment.weights=use.augment.weights, use.full.null=use.full.null, added.data.points=added.data.points,
+                            use.multi.impute=use.multi.impute, num.imp=num.imp, seed=scan.seed,
+                            weights=weights,
                             ...)
     if(keep.full.scans){
       full.results[i,] <- this.scan$p.value
