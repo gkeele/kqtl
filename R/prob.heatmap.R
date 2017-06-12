774d420bd543bc3a3a93031221fd1ce2083b065b
Greg Keele <gkeele@gmail.com>
2017-06-12 18:38:03 -0400

    Added some options to prob.heatmap()

--- R/prob.heatmap.R
+++ R/prob.heatmap.R
@@ -14,10 +14,15 @@
 #' @param merge.by DEFAULT: "SUBJECT.NAME". Specifies the columns to merge phenotype and haplotype data.
 #' @param founder.labels DEFAULT: "NULL". If NULL, will default to the labels in the genome cache.
 #' @param founder.cex DEFAULT: 1. Defines the text size of the founder labels.
+#' @param include.ramp DEFAULT: TRUE. If TRUE, spectrum ramp for dosages or probabilities is included.
+#' @param include.marker DEFAULT: TRUE. If TRUE, the marker name is included in the title.
+#' @param alternative.phenotype.label DEFAULT: NULL. Allows for an alternative label for the phenotype.
 #' @export
 #' @examples prob.heatmap()
 prob.heatmap = function(marker, p.value=NULL, genomecache, model="additive",
-                        phenotype, phenotype.data, merge.by="SUBJECT.NAME", founder.labels=NULL, founder.cex=1){
+                        phenotype, phenotype.data, merge.by="SUBJECT.NAME", founder.labels=NULL, founder.cex=1,
+                        include.ramp=TRUE, include.marker=TRUE,
+                        alternative.phenotype.label=NULL){
   if(!is.null(p.value)){
     p.value <- round(-log10(p.value), 4)
   }
@@ -55,7 +60,6 @@ prob.heatmap = function(marker, p.value=NULL, genomecache, model="additive",
   cols <- rev(gray(10000:1/10000))
   par(plt=c(0.1,.75,0.1,.8))    ##set the margin  
   image(z=1-probs, axes=FALSE, col=cols)
-  #heatmap(t(probs), Rowv=NA, Colv=NA, col = rev(cols), scale="column", margins=c(4,4), labCol="") 
   box()
   axis(2, at=seq(0, num.col, 1+1/num.col)/num.col, 
        labels=rev(founder.labels), cex.axis=founder.cex,
@@ -65,16 +69,18 @@ prob.heatmap = function(marker, p.value=NULL, genomecache, model="additive",
   this.title <- ifelse(is.null(p.value), marker, paste0(marker, ": -log10P=", p.value))
   title(this.title, line=2.5)
   
-  ramp.label <- ifelse(model == "additive", "Dose", "Prob")
-  par(fig=c(.9,.97,.3,.6), mai=c(.1,.1,.3,.1), new=TRUE)
-  if(model == "additive"){ image(y=seq(from=0, to=2, length.out=length(cols)), z=matrix(seq(from=0, to=2, length.out=length(cols)), nrow=1), 
-                                 zlim=c(0, 2), ylim=c(0, 2), axes=FALSE, col=rev(cols), main=ramp.label) } #for the legend 
-  if(model == "full"){ image(y=seq(from=0, to=1, length.out=length(cols)), z=matrix(seq(from=0, to=1, length.out=length(cols)), nrow=1), 
-                             zlim=c(0, 1), ylim=c(0, 1), axes=FALSE, col=rev(cols), main=ramp.label) }
-  box()
-  axis(2, las=1)          
-  
-  par(op)
+  if(include.ramp){
+    ramp.label <- ifelse(model == "additive", c("Haplotype", "Dose"), c("Diplotype", "Prob"))
+    par(fig=c(.9,.97,.3,.6), mai=c(.1,.1,.3,.1), new=TRUE)
+    if(model == "additive"){ image(y=seq(from=0, to=2, length.out=length(cols)), z=matrix(seq(from=0, to=2, length.out=length(cols)), nrow=1), 
+                                   zlim=c(0, 2), ylim=c(0, 2), axes=FALSE, col=rev(cols), main=ramp.label) } #for the legend 
+    if(model == "full"){ image(y=seq(from=0, to=1, length.out=length(cols)), z=matrix(seq(from=0, to=1, length.out=length(cols)), nrow=1), 
+                               zlim=c(0, 1), ylim=c(0, 1), axes=FALSE, col=rev(cols), main=ramp.label) }
+    box()
+    axis(2, las=1)          
+    
+    par(op)
+  }
 }
 
 #' @export
@@ -114,7 +120,9 @@ prob.image = function(marker.data, marker=NULL, p.value=NULL,
        lty=0, srt=90, las=2) # add txt on the strain   
   axis(1, at=0.5, labels=phenotype, tick=FALSE)
   axis(3, at=c(0,0.25,0.5,0.75,1), labels=c(s1,s2,s3,s5,s6))
-  this.title <- ifelse(is.null(p.value), marker, paste0(marker, ": -log10P=", p.value))
+  if(include.marker){
+    this.title <- ifelse(is.null(p.value), marker, paste0(marker, ": -log10P=", p.value))
+  }
   title(this.title, line=2.5)
   par(plt <- oplt)
 }
